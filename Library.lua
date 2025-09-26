22166dda8e224b48f5ca912faf3297eb92e370d7
nymus777 <137441255+nymus573l@users.noreply.github.com>
2025-09-27 00:29:59 +0200

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -104,17 +104,16 @@
 	local config_flags = library.config_flags
 
 	local themes = {
-	preset = {
-		["outline"] = hex("#0A0A0A"),
-		["inline"] = hex("#2D2D2D"),
-		["accent"] = hex("#E6D6FF"), -- white purple
-		["high_contrast"] = hex("#141414"),
-		["low_contrast"] = hex("#1E1E1E"),
-		["text"] = hex("#B4B4B4"),
-		["text_outline"] = rgb(0, 0, 0),
-		["glow"] = hex("#E6D6FF"), -- white purple
-	},
-}
+		preset = {
+			["outline"] = hex("#0A0A0A"),
+		    ["inline"] = hex("#2D2D2D"),
+		    ["accent"] = hex("#E6D6FF"), -- white purple
+		    ["high_contrast"] = hex("#141414"),
+		    ["low_contrast"] = hex("#1E1E1E"),
+		    ["text"] = hex("#B4B4B4"),
+		    ["text_outline"] = rgb(0, 0, 0),
+		    ["glow"] = hex("#E6D6FF"), -- white purple
+		},
 
 		utility = {
 			["outline"] = {
